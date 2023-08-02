332679eadd13f649d9cd88adb4b70fc99e4c67a8
Amulyasagar <66465132+Amulyasagar@users.noreply.github.com>
2023-08-02 19:05:01 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,21 @@
 */
 
 function isAnagram(str1, str2) {
+  var strarr1 = str1.split("").sort();
+  var strarr2 = str2.split("").sort();
+  console.log(strarr1.join(""));
+  console.log(strarr2.join(""));
+
+  sortstr1 =strarr1.join("");
+  sortstr2 =strarr1.join("");
+
+
+  if(sortstr1==sortstr2)
+  return true;
+  else
+  return false;
 
 }
-
+var res= isAnagram("name","mane");
+console.log(res);
 module.exports = isAnagram;
